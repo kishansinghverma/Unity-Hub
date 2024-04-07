001c86cc70c5628ed4958eaffcc7438db827d1f6
kishansinghverma <kishansinghverma@gmail.com>
2024-04-07 11:39:35 +0530

    Fixed Sddress

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import splitwiseRoute from './backend/routes/splitwise';
 import dotenv from 'dotenv';
 import { Logger } from './backend/common/models';
 import { source, style } from './backend/common/constants';
-import path from 'path';
 
 dotenv.config();
 const app = express();
 const port = parseInt(process.env.PORT || '8080');
+const address = process.env.ADDRESS || 'localhost';
 const logger = new Logger(source.server);
 
 app.use('/emandi', emandiRoute);
 app.use('/splitwise', splitwiseRoute);
 app.all("*", (req, res) => res.status(404).end());
-app.listen(port, () => logger.success(`Unity server is live on ${port}! 🎉`, style.bold));
+app.listen(port, address, () => logger.success(`Unity server is live on ${port}! 🎉`, style.bold));
